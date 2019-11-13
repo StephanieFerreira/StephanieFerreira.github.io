d4fa9daf1cede19cb84ce9ccdad8a9aff2f251fd
Stephanie <stephanie.ferreira@gmx.de>
2019-11-13 22:28:30 +0100

    countdown

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const calendarButton = document.querySelector(".button-test");
 const calendarContainer = document.querySelector(".container");
+const firstLine = document.querySelector(".sentence")
 
 const calendarDays = 24;
 
@@ -23,7 +24,7 @@ const createCalendar = () => {
 		calendarContainer.appendChild(calendarDoor);
 		
 		var today = new Date();
-		if (today.getDate() > i ){
+		if (today.getDate() > i && today.getMonth() == 11){
 			pdf_path = `./${num}.pdf`
 			pdf_path = './docs/test.pdf'
 			calendarDoor.addEventListener("click", openDoor.bind(null,pdf_path))
@@ -31,14 +32,25 @@ const createCalendar = () => {
 	}
 }
 
+const createFirstLine = () => {
+	today=new Date();
+	var cmas=new Date(today.getFullYear(), 11, 24);
+	if (today.getMonth()==11 && today.getDate()>24) 
+	{
+		cmas.setFullYear(cmas.getFullYear()+1); 
+	}  
+	var one_day=1000*60*60*24;
+	n = Math.ceil((cmas.getTime()-today.getTime())/(one_day))
+	firstLine.innerHTML = "Noch <div style=\"display: inline\", class=\"number\">" + n + "</div> Tage bis Weihnachten"
+}
+
 function save(){
 	var object = new ActiveXObject("Scripting.FileSystemObject");
-	var file = object.CreateTextFile("C:\\Hello.txt", false);
+	var file = object.CreateTextFile("C:/Users/IBM_ADMIN/Desktop/Test/Hello.txt", false);
 	file.WriteLine('Hello World');
 	file.WriteLine('Hope is a thing with feathers, that perches on the soul.'); 
 	file.Close();
 }
 
-
+createFirstLine()
 createCalendar()
-save()
